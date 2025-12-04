5ea05298ae5df73b7f95da8d506079bd48326680
weavermarquez <weavermarquez0+github@gmail.com>
2025-12-04 23:14:48 +0000

    Define Mention DocType

---
+++ promethean_soup/promethean_soup/doctype/mention/mention.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Valerie Kim and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Mention(Document):
+	pass
---
+++ promethean_soup/promethean_soup/doctype/mention/test_mention.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Valerie Kim and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMention(FrappeTestCase):
+	pass
